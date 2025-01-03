512dbb286a84bfe5d52242754288c9333334f4cb
tharusha <theenutheekshana@gmail.com>
2025-01-04 01:31:36 +0530

    Add required dependencies and assets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_node/sreens/home_screen.dart';
+import 'package:weather_node/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,18 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen()
-    );
+        title: 'WeatherNode',
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+            fontFamily: "Poppins"),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen());
   }
 }
-
